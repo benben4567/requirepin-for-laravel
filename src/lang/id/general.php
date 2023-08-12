e208e29156fe4585821fd454034498c0cf5db73c
benben4567 <benben4567@gmail.com>
2023-08-12 13:58:35 +0700

    feat: add Indonesian lang translation

---
+++ src/lang/id/general.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'success' => 'sukses',
+    'fail' => 'gagal',
+    'no_changes' => 'Tidak ada perubahan',
+    'unknown_error' => 'Terjadi kesalahan',
+    'not_found' => 'Tidak ditemukan',
+
+];
---
+++ src/lang/id/pin.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'default' => 'Anda harus mengganti pin saat ini. Anda tidak diperbolehkan menggunakan pin bawaan!',
+    'require_pin' => 'Masukkan pin untuk melanjutkan!',
+    'terminated' => 'Pengguna sedang tidak dalam sesi log-in!',
+    'expired_url' => 'URL telah kedaluwarsa!',
+    'invalid_url' => 'URL tidak ditemukan!',
+    'changed' => 'Pin anda telah diganti!',
+    'wrong' => 'Anda memasukkan pin yang salah!',
+    'throttle' => 'Terlalu banyak percobaan. Coba lagi dalam :seconds detik.',
+    'unverified_sender' => 'Pengirim tidak dapat diverifikasi!',
+    'unknown_error' => 'Terjadi kesalahan! Anda tidak dapat mengganti pin saat ini!',
+    'exists' => '{0} Anda tidak diizinkan menggunakan pin yang ada atau pin sebelumnya!|{1} Anda tidak diizinkan menggunakan :number pin terakhir atau pin saat ini!',
+    'notify' => [
+        'subject' => 'Pin anda telah diganti.',
+        'introduction' => 'Pin anda berhasil diganti!',
+        'message' => 'Jika tindakan ini tidak dilakukan oleh anda, silahkan klik tautan dibawah ini untuk mengganti pin anda.', // .
+        'action' => 'Klik Disini',
+        'complimentary_close' => 'Hati-hati!'
+    ]
+
+];
